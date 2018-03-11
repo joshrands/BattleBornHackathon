a40ea094c23c7053cf4e80c992cd7e4a4c1d1f7e
Seth Jacob Asadi <sethasadi@MacBook-Pro-Revamped.local>
2018-03-10 21:11:28 -0800

    Add start setuo

--- InstallUI.java
+++ InstallUI.java
@@ -13,7 +13,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Timer;
 
-public class DashboardUI extends Canvas implements Runnable, MouseListener {
+public class InstallUI extends Canvas implements Runnable, MouseListener {
 
 	private Thread thread;
 	private BufferStrategy bs;
@@ -23,32 +23,35 @@ public class DashboardUI extends Canvas implements Runnable, MouseListener {
 
 	private int screen;
 
-	private BufferedImage DashUI;
-	private BufferedImage DashUILU;
-	private BufferedImage DashUIRU;
-	private BufferedImage DashUILD;
-	private BufferedImage DashUIRD;
+	private BufferedImage SetupUI;
+	
+	private JTextField id;
 
-	public DashboardUI() {
+	public InstallUI() {
 		//addMouseMotionListener(this);
 		addMouseListener(this);
 
 		try {
-			DashUI = ImageIO.read(new File ("UI.png"));
-			DashUILU = ImageIO.read(new File ("UILU.png"));
-			DashUIRU = ImageIO.read(new File ("UIRU.png"));
-			DashUILD = ImageIO.read(new File ("UILD.png"));
-			DashUIRD = ImageIO.read(new File ("UIRD.png"));
+			SetupUI = ImageIO.read(new File ("UI.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+		id = new JTextField(1);
+		
+        id.setBounds(68, 719, 1891, 115);
+        //add(id);
+        
 		running = false;
 
-		screen = 1;
+		screen = 0;
 	}
-
+	
+	public JTextField getID() {
+		return id;
+	}
+	
 	public void render() {
 		//this is some bs...
 		//hahahaha get it
@@ -70,20 +73,9 @@ public class DashboardUI extends Canvas implements Runnable, MouseListener {
 		g.setColor(Color.black);
 		//g2.draw(test);
 		if (screen == 0) {
-			//g.drawImage(DashUI, 0, 0, getWidth(), getHeight(), null);
-		} else if (screen == 1) {
-			g.drawImage(DashUI, 0, 0, getWidth(), getHeight(), null);
-		} else if (screen == 5) {
-			g.drawImage(DashUILU, 0, 0, getWidth(), getHeight(), null);
-		} else if (screen == 6) {
-			g.drawImage(DashUIRU, 0, 0, getWidth(), getHeight(), null);
-		} else if (screen == 7) {
-			g.drawImage(DashUILD, 0, 0, getWidth(), getHeight(), null);
-		} else if (screen == 8) {
-			g.drawImage(DashUIRD, 0, 0, getWidth(), getHeight(), null);
-		}
-
-
+			g.drawImage(SetupUI, 0, 0, getWidth(), getHeight(), null);
+		} 
+		
 		//cam cam said so, I guess wipe the screen?
 		bs.show();
 		g.dispose();
@@ -91,7 +83,7 @@ public class DashboardUI extends Canvas implements Runnable, MouseListener {
 
 	public static void main(String[] args) {
 		//new JFrame
-		JFrame win = new JFrame("Barricks Dashboard");
+		JFrame win = new JFrame("Barricks Setup");
 		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		win.setSize(1024, 768);
@@ -102,7 +94,7 @@ public class DashboardUI extends Canvas implements Runnable, MouseListener {
 		//black background
 		ui.setBackground(Color.BLACK);
 
-
+		win.add(ui.getID());
 
 		//add the instance
 		win.add(ui);
@@ -147,64 +139,23 @@ public class DashboardUI extends Canvas implements Runnable, MouseListener {
 		// TODO Auto-generated method stub
 		int x = e.getX() * 2;
 		int y = e.getY() * 2;
-		if (screen == 1) {
-			// this is the moron dashboard  
-			// send workOrder.txt to raspberry pi
-			if (x > 76 && x < 983) {
-				if (y > 605 && y < 893) {
-					screen = 5;
-				}
-				if (y > 967 && y < 1255) {
-					screen = 7;
-				}
-			}
-			if (x > 1105 && x < 1970) {
-				if (y > 605 && y < 893) {
-					screen = 6;
-				}
-				if (y > 967 && y < 1255) {
-					screen = 8;
-				}
-			}
-		} else if (screen == 5 || screen == 6 || screen == 7 || screen == 8) {
-			if (x > 76 && x < 983) {
-				if (y > 605 && y < 893) {
-					screen = 5;
-				}
-				if (y > 967 && y < 1255) {
-					screen = 7;
-				}
-			}
-			if (x > 1105 && x < 1970) {
-				if (y > 605 && y < 893) {
-					screen = 6;
-				}
-				if (y > 967 && y < 1255) {
-					screen = 8;
-				}
-			}
-			if (x > 506 && x < 1538 && y > 1306 && y < 1487) {
-				System.out.println("Sending work order to Oracle database...");
+		if (screen == 0) {
+			if (x > 1828 && x < 1967 && y > 717 && y < 837) {
+				System.out.println("Finishing Bridge Box Setup...");
 
 				try {
-					PrintWriter out = new PrintWriter("workOrder.txt");
-					out.println("request");
-					System.out.println("Data written.");
+					PrintWriter out = new PrintWriter("oracle.txt");
+					out.println("installed");
+					System.out.println("New Asset Setup");
 					out.close();
 				} catch(FileNotFoundException ex) {
 					System.out.println(ex);
 				}
-				if (screen == 8) {
-					screen = 2;
-				} else {
-					screen = 3;
-				}
+				
+				//quit
+				System.exit(0);
 			}
-		} else if (screen == 2) {
-			// "Other" W.O. option pulls up forge app and eventually does the submit from above
-		} else if (screen == 3) {
-			// Maintenance guy screen
-		}  
+		}
 	}
 
 	@Override
