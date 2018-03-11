b84e3428d22c0e8074098dc51048d3135dc9d9ea
joshrands <jmrands@mines.edu>
2018-03-10 22:23:01 -0800

    Demo order changed

--- demo.sh
+++ demo.sh
@@ -1,26 +1,4 @@
 #bash src/run.sh
-
-# Installation Demo
-rm install.txt # install.txt tells GUI that we are installing
-rm oracle.txt
-touch oracle.txt
-
-waiting=true
-while ${waiting}
-do
-	echo "Please attach BridgeBox..."
-	if [ -a "install.txt" ]
-	then
-		echo "BridgeBox attached, enter Asset iD:"
-		waiting=false
-	fi
-	sleep 1
-done
-
-installUI='InstallUI'
-javac ${installUI}.java
-java ${installUI}
-
 # TODO: Maybe add a condition for completing installation here.
 #while true;
 #do
@@ -51,4 +29,26 @@ done
 
 javac ${javaUI}.java
 java ${javaUI}
+
+# Installation Demo
+rm install.txt # install.txt tells GUI that we are installing
+rm oracle.txt
+touch oracle.txt
+
+waiting=true
+while ${waiting}
+do
+	echo "Please attach BridgeBox..."
+	if [ -a "install.txt" ]
+	then
+		echo "BridgeBox attached, enter Asset iD:"
+		waiting=false
+	fi
+	sleep 1
+done
+
+installUI='InstallUI'
+javac ${installUI}.java
+java ${installUI}
+
 
